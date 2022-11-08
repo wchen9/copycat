45c3cb395130e05f9dcadad62866db6960b18520
wchen9 <i@wangc.org>
2022-11-08 10:54:16 +0800

    spring 使用注解内嵌容器启动 controller

---
+++ heima/src/main/java/org/example/a02/A02.java
@@ -0,0 +1,44 @@
+package org.example.a02;
+
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class A02 {
+
+    public static void main(String[] args) {
+        AnnotationConfigServletWebServerApplicationContext context = new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+    }
+
+    @Configuration
+    static class WebConfig {
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory() {
+            return new TomcatServletWebServerFactory();
+        }
+
+        @Bean
+        public DispatcherServlet dispatcherServlet() {
+            return new DispatcherServlet();
+        }
+
+        @Bean
+        public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet) {
+            return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        }
+
+        @Bean("/hello")
+        public Controller controller1() {
+            return (request, response) -> {
+                response.getWriter().print("hello");
+                return null;
+            };
+        }
+    }
+
+}
