ed75b87a2f12655a45d1759f8d0b6de0b5f1b35f
wchen9 <i@wangc.org>
2022-12-06 17:54:55 +0800

    change default port and add hello log print

--- springboot-demo/src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ springboot-demo/src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.springbootdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class HelloController {
 
     @RequestMapping("/01")
     public String doHello(){
+        log.info("print hello ");
         return "hello";
     }
 
